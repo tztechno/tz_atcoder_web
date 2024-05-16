01ad3e6240bf958ac5139ce23261c3f126018a03
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-16 16:13:54 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ContestForm />
+  </div>
+</template>
+
+<script>
+import ContestForm from './components/ContestForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ContestForm
+  }
+};
+</script>
+
+<style>
+/* 必要に応じてスタイルを追加します */
+</style>
