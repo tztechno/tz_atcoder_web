17ff6a9b3c5acd1be2f2dc2a8b654e1c7b57fde5
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-06-10 16:37:57 +0900

    Delete abc053a_laravel directory

--- abc053a_laravel/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RateController;
-
-Route::get('/', [RateController::class, 'index']);
-Route::post('/', [RateController::class, 'store']);
