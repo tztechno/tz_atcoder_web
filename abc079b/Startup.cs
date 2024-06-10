b5c0d9c420e26a90bbbf1874e38118a8c4485a99
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-06-10 16:32:49 +0900

    Add files via upload

---
+++ abc079b/Startup.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nancy.Owin;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseOwin(x => x.UseNancy());
+    }
+}
